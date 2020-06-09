f6a18b672bd2419a69ffd8e0b8283ff99659b210
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-06-09 14:36:03 +0300

    halving n/2 then sum the halves

---
+++ halvingSum.js
@@ -0,0 +1,18 @@
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+function halvingSum(n) {
+    var Sum = 0;
+    while (n > 0) {
+        Sum += n;
+        n = Math.floor(n / 2);
+    }
+    return Sum;
+}
+console.log(halvingSum(27));
+// 27 => 27 + 13 + 6 + 3 + 1 = 50
