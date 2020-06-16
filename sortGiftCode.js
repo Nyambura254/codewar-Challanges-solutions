d2d63a620c52f30866280e5167429244ac7c16a5
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-16 15:01:46 +0300

    Sort the Gift Code

---
+++ sortGiftCode.js
@@ -0,0 +1,21 @@
+function sortGiftCode(code) {
+    var result = [];
+    for (var i = 0; i < code.length; i++) {
+        result.push(code.substr(i, 1));
+    }
+    return result.sort().join('');
+}
+console.log(sortGiftCode('gwqieeudnd'));
+
+// step 2
+// function sortGiftCode(code){
+//     return code.split('').sort().join('');
+//   }
+
+// step 3
+// const sortGiftCode = code => code.split('').sort().join('');
+
+//step 4
+// function sortGiftCode(code){
+//     return code.split('').sort().toString().replace(/,/g,''); 
+//   }
