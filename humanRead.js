9e27f34bbfc41662887692e9179b983709432fa9
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-17 22:56:42 +0300

    update on finding wats to get time

--- humanRead.js
+++ humanRead.js
@@ -1,5 +1,7 @@
 // write a function, which takes a non - negative integer(seconds) as input and returns the time in a human - readable format(HH: MM: SS)
 
+
+// example
 // HH = hours, padded to 2 digits, range: 00 - 99
 // MM = minutes, padded to 2 digits, range: 00 - 59
 // SS = seconds, padded to 2 digits, range: 00 - 59
