94908aeedfeef68c7ebb18df8c3ee55365794769
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-17 22:46:59 +0300

    update on bitcount

--- bitCount.js
+++ bitCount.js
@@ -3,8 +3,7 @@
 
 // Example: The binary representation of 1234 is 10011010010, so the
 // function should
-// return 5 in this
-// case
+// return 5 in this case
 
 var countBits = function(n) {
     // make an array with the bit result
