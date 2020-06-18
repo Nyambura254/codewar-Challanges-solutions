4062ab41152dc44f251e8941ceffc62588f243d6
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-18 10:20:56 +0300

    domain split

--- DomainSplit.js
+++ DomainSplit.js
@@ -1,4 +1,9 @@
-// step 1
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+// // step 1
 // domainName = url => url.replace("https://", "").replace("http://", "").replace("www.", "").split(".")[0];
 
 //example 2
