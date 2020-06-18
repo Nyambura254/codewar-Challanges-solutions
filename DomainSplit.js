79070e967ece57089e8f3a95d6777ef7ca3bc7ae
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-18 10:19:42 +0300

    domain split

--- DomainSplit.js
+++ DomainSplit.js
@@ -5,3 +5,19 @@
 function domainName(url) {
     return url.split('/').filter(it => it.length > 0 && it.indexOf('http') === -1)[0].split('.').filter(it => it !== 'www')[0]
 }
+
+//EXAMPLE 3
+function domainName(url) {
+    var urlSplit = url.split("/");
+    for (let i of urlSplit) {
+        if (i.includes(".")) {
+            var UpdatedSplit = i.split(".")
+            for (let j of UpdatedSplit) {
+                if (j == 'www') {
+                    continue;
+                }
+                return j
+            }
+        }
+    }
+}
