f9f264a641c25441af6c08ee7c354465f7619885
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-19 17:27:40 +0300

    filter server

--- filter.js
+++ filter.js
@@ -1,4 +1,5 @@
-//programing what is done behide server on filter search f the elements to be sorted when you try to search
+//programing what is done behide server on filter search f the elements to be sorted when you try to search with given keywords
+
 Array.prototype.filter = function(fn) {
     const newArray = [];
     for (let i = 0; i < this.length; i++) {
