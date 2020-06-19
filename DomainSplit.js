dbdf8b68b060dfa0c97d098d0b3b8e22797f4212
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-19 17:05:41 +0300

    domain split

--- DomainSplit.js
+++ DomainSplit.js
@@ -13,21 +13,6 @@
 
 //example 2
 function domainName(url) {
-    return url.split('/').filter(it => it.length > 0 && it.indexOf('http') === -1)[0].split('.').filter(it => it !== 'www')[0]
+    url.replace("https://", "").replace("http://", "").replace("www.", "").split(".")[0];
 }
-
-//EXAMPLE 3
-function domainName(url) {
-    var urlSplit = url.split("/");
-    for (let i of urlSplit) {
-        if (i.includes(".")) {
-            var UpdatedSplit = i.split(".")
-            for (let j of UpdatedSplit) {
-                if (j == 'www') {
-                    continue;
-                }
-                return j
-            }
-        }
-    }
-}
+console.log(domainName("http://www.zombie-bites.com"));
