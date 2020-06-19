35d01db59491f88208eb725eafe9f894d8cf380b
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-19 17:28:13 +0300

    filter server

--- filter.js
+++ filter.js
@@ -1,6 +1,6 @@
 //programing what is done behide server on filter search f the elements to be sorted when you try to search with given keywords
 
-Array.prototype.filter = function(fn) {
+Array.prototype.filter = function(fn) { //our function intialization
     const newArray = [];
     for (let i = 0; i < this.length; i++) {
         if (fn(this[i])) newArray.push(this[i])
