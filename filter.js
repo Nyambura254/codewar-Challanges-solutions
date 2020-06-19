65eb561daf7742604efe71474790b1d2bb43794b
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-19 17:29:25 +0300

    filter server

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@
 
 Array.prototype.filter = function(fn) { //our function intialization
     const newArray = []; //our array to be empty
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) { //we setup,do the comparison and give our change
         if (fn(this[i])) newArray.push(this[i])
     }
     return newArray
