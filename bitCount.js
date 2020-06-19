8ca44ee905a57511c34ecc91d7828d4578a927b7
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-19 17:32:29 +0300

    counting bit

--- bitCount.js
+++ bitCount.js
@@ -12,5 +12,5 @@ var countBits = function(n) {
     // make a sum with the array and make the index a Number
     const result = base.reduce((sum, num) => sum + Number(num), 0);
 
-    return result;
+    return result; //return result
 };
