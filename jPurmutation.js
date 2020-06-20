7cbfc7b378e062e73fa8b777c97e0c9650bfa452
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-20 12:41:26 +0300

    josephus permutation

--- jPurmutation.js
+++ jPurmutation.js
@@ -24,7 +24,7 @@
 
 function josephus(items, k) { //function initialization with two parameters
     let count = 0; //start our count at zero
-    let result = [];
+    let result = []; //empty array as our results
     while (items.length > 0) {
         for (let i = 0; i < items.length; i++) {
             if (++count % k === 0) { //our count start at 0,we increment it....the % is what we have in our sequence[...] and k is what we want to be sorted and that person to kill himself
