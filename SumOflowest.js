2529bc2c2bc285d89a051d736e8a55436fe7c74b
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-25 15:18:08 +0300

    adding two lowest values

--- SumOflowest.js
+++ SumOflowest.js
@@ -3,7 +3,8 @@ Create a function that returns the sum of the two lowest positive
 numbers given an array of minimum 4 positive integers. No floats or 
 non-positive integers will be passed.
 */
-function sumTwoSmallestNumbers(numbers) {
+function sumTwoSmallestNumbers(numbers) { //function initialization
+
     //Code here
     let number = numbers.sort(function(a, b) {
         return a - b;
