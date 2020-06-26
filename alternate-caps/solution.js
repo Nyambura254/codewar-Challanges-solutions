dbe1ebb3d4033a2deec24b5e6ef080dafe6c9c39
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-26 16:26:14 +0300

    initial commits

--- alternate-caps/solution.js
+++ alternate-caps/solution.js
@@ -19,7 +19,7 @@ function capitalize(s) { //initialization
         if (i % 2 === 1) { //if divided and has reminder
             oddStr += s[i].toUpperCase(); //increment and do uppercase
         } else {
-            oddStr += s[i];
+            oddStr += s[i]; //gives output
         }
     }
     return [evenStr, oddStr];
