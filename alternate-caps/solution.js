308e22fe502981aac58567f0950785654aa8a7d3
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-26 15:57:05 +0300

    initial commits

---
+++ alternate-caps/solution.js
@@ -0,0 +1,27 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+'use strict'
+
+function capitalize(s) {
+    let evenStr = '';
+    let oddStr = '';
+
+    for (let i = 0; i < s.length; i++) {
+        if (i % 2 === 0) {
+            evenStr += s[i].toUpperCase();
+        } else {
+            evenStr += s[i];
+        }
+
+        if (i % 2 === 1) {
+            oddStr += s[i].toUpperCase();
+        } else {
+            oddStr += s[i];
+        }
+    }
+    return [evenStr, oddStr];
+};
+console.log(capitalize("abcdef"));
---
+++ quarterOfyr.js
@@ -0,0 +1,20 @@
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+// 
+const quarterOf = (month) => {
+    let quarter;
+    if (month < 4) //first quarter
+        return 1;
+    else if (month < 7) //second quarter
+        return 2;
+    else if (month < 10) //third quarter
+        return 3;
+    else if (month < 13) //fourth quarter
+        return 4;
+    else if (month < 14) //anything above 12 
+        return quarter; //gives undefined
+    // console.log("In a yr we have 12 months"); //or undefined
+
+}
+console.log(quarterOf(13));
