06cb4ce1afd3da017c1ce57a53d2e328cf65105b
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-29 23:34:13 +0300

    finding average

--- average.js
+++ average.js
@@ -1,6 +1,7 @@
 // Write function avg which calculates average of numbers in given list.
 // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript-----find the challenge here
 function avg(array) { //initialization
+    //The reduce() method reduces the array to a single value. The reduce() method executes a provided function for each value of the array (from left-to-right)
     let sum = array.reduce((a, b) => a + b);
     return sum / array.length;
 }
