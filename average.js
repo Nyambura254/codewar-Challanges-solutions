a2fb14586edec9074cfc189c946f413bad184238
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-29 23:35:46 +0300

    finding average

--- average.js
+++ average.js
@@ -3,6 +3,6 @@
 function avg(array) { //initialization
     //The reduce() method reduces the array to a single value. The reduce() method executes a provided function for each value of the array (from left-to-right)
     let sum = array.reduce((a, b) => a + b); //after we pass a and b,we sum
-    return sum / array.length;
+    return sum / array.length; //our sum we divide by length of numbers passed
 }
 console.log(avg([2, 4, 1, 5]));
