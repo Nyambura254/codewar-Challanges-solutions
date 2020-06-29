ff1a74d9c8e02fe4e360da6377e7349969c6a019
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-29 23:25:25 +0300

    initial commit

--- solution.js
+++ solution.js
@@ -9,7 +9,7 @@ function capitalize(s) { //initialization
     let evenStr = ''; //we can have strings not odd....
     let oddStr = ''; //we can have strings odd....
 
-    for (let i = 0; i < s.length; i++) { //setup,comparison,change
+    for (let i = 0; i < s.length; i++) {
         if (i % 2 === 0) { //if its even
             evenStr += s[i].toUpperCase(); //increment  what we passed with [i] and return uppercase string
         } else {
