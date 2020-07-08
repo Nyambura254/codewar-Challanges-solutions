595b2910a8eea4c378f028fae615b2a28f510282
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-08 17:52:45 +0300

    multiples of 3 &5

--- kata1.js
+++ kata1.js
@@ -5,7 +5,7 @@
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
 function solution(number) { //initializations
-    sum = 0
+    sum = 0; //start at zero
     for (i = 3; i < number; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             sum += i;
